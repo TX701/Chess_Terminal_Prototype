ecd103e999816e5d5d3bda3f475a681dbd60138d
Rory <167032554+TX701@users.noreply.github.com>
2025-03-29 14:40:44 -0400

    Add files via upload

--- Board.java
+++ Board.java
@@ -30,6 +30,7 @@ public class Board {
         int newCol = start.col + colDiff;
         int newRow = start.row + rowDiff;
 
+
         if (-1 < newCol && newCol < 8 && -1 < newRow && newRow < 8) {
             return board[newRow][newCol];
         }
@@ -40,11 +41,11 @@ public class Board {
     public void moveManager(Tile tile) {
         switch (tile.piece.type) {
             case "Rook" -> pawnMovement(tile);
-            //case "Knight" -> kingMovement(tile.piece);
-            //case "Bishop" -> bishopMovement(tile.piece);
-            //case "Queen" -> queenMovement(tile.piece);
-            //case "King" -> kingMovement(tile.piece);
-            //case "Pawn" -> pawnMovement(tile.piece);
+            case "Knight" -> kingMovement(tile);
+            case "Bishop" -> bishopMovement(tile);
+            case "Queen" -> queenMovement(tile);
+            case "King" -> kingMovement(tile);
+            case "Pawn" -> pawnMovement(tile);
             default -> {
             }
         }
@@ -58,7 +59,7 @@ public class Board {
         Tile diagonalLeft = tileMath(tile_with_pawn, -1, rowMovement); // left with perspective of the board not the piece
         // if the diagonal left is not off the board and if the diagonal left is an emeny
         if (diagonalLeft != null && diagonalLeft.piece != null) {
-            if (diagonalLeft.piece.side != tile_with_pawn.piece.side) {
+            if (diagonalLeft.piece.side != tile_with_pawn.piece.side && diagonalLeft.piece.side != 'N') {
                 possibleMoves.add(diagonalLeft);
             }
         }
@@ -66,7 +67,7 @@ public class Board {
         Tile diagonalRight = tileMath(tile_with_pawn, 1, rowMovement); // right with perspective of the board not the piece
         // if the diagonal right is not off the board and if the diagonal right is an emeny
         if (diagonalRight != null && diagonalRight.piece.type != null) {
-            if (diagonalRight.piece.side != tile_with_pawn.piece.side) {
+            if (diagonalRight.piece.side != tile_with_pawn.piece.side && diagonalRight.piece.side != 'N') {
                 possibleMoves.add(diagonalRight);
             }
         }
@@ -86,135 +87,119 @@ public class Board {
 
         return possibleMoves;
     }
-
+    
     // tool for getting a line 
     public List<Tile> getLine(Tile start, int colInc, int rowInc) {
         List<Tile> line = new ArrayList<>();
 
-        int curCol = colInc;
-        int curRow = rowInc;
+        boolean knight = (start.piece.type != null) ? (start.piece.type.equals("Knight")) : false;
 
-        int inc = 1;
-        while (tileMath(start, 0, inc) != null) {
-            
-            System.out.println(tileMath(start, 0, inc).location);
-            inc--;
-            
+        Tile newTile = tileMath(start, colInc, rowInc);
+        while (newTile != null) {
+
+            if (newTile.piece.type == null || knight) {
+                line.add(newTile);
+                newTile = tileMath(newTile, colInc, rowInc);
+            } else if (newTile.piece.side != start.piece.side && newTile.piece.side != 'N') {
+                line.add(newTile);
+                return line;
+            } else {
+                return line;
+            }
         }
-        
-
-        // // if the piece is within board range
-        // while (nextTile != null) {
-        //     System.out.println(curCol + " " + curRow);
-        //     // we use this method as a helper for the knight but for the knight we need to not stop if we 'run into' a piece so we keep track of if the piece is a knight here
-        //     boolean knight = (nextTile.piece.type != null) ? (nextTile.piece.type.equals("Knight")) : false;
-
-        //     // if the tile is empty or if we are collecting tiles for the knight
-        //     if (nextTile.piece.type == null || knight) {
-        //         line.add(nextTile);
-
-        //         curCol = curCol + colInc; // the col val will be -1 1 or 0 this method will either do nothing, increase by 1, or decrease by one
-        //         curRow = curRow + rowInc; // the row val will be -1 1 or 0 this method will either do nothing, increase by 1, or decrease by one
-
-        //         nextTile = tileMath(nextTile, curCol, curRow);
-        //     // if the tile is occupied by an enemy
-        //     } else if ((nextTile.piece.side == 'W' && start.piece.side == 'B') || (nextTile.piece.side == 'B' && start.piece.side == 'W')) {
-        //         line.add(nextTile); // add to the possible moves but stop collecting the line
-        //         break;
-        //     } else {
-        //         break;
-        //     }
-        // }
 
         return line;
     }
 
-    // public void getPlus(String location, List<String> list) {
-    //     getLine(location, list, 1, 0); // go to the right
-    //     getLine(location, list, -1, 0); // go to the left
-    //     getLine(location, list, 0, 1); // go up
-    //     getLine(location, list, 0, -1); // go down
-    // }
+    public List<Tile> getPlus(Tile start) {
+        List<Tile> lines = new ArrayList<>();
 
-    // public void getCross(String location, List<String> list){
-    //     getLine(location, list, 1, 1); // right up
-    //     getLine(location, list, 1, -1); //  right down
-    //     getLine(location, list, -1, 1); // left up
-    //     getLine(location, list, -1, -1); // left down
-    // }
+        lines.addAll(getLine(start, 1, 0)); // go to the right
+        lines.addAll(getLine(start, -1, 0)); // go to the left
+        lines.addAll(getLine(start, 0, 1)); // go up
+        lines.addAll(getLine(start, 0, -1)); // go down
 
-    // // gets a square from the perspective of the location
-    // public void getSquare(String location, List<String> list, int start, int end) {
-    //     for (int col = start; col < end; col++) {
-    //         for (int row = start; row < end; row++) {
-    //             String tile = tileMath(location, col, row);
-    //             if (tile != null && tile != location) {
-    //                 if (tileToPiece(tile) == null || (tileToPiece(tile) != null && (tileToPiece(tile).side) != tileToPiece(location).side)) {
-    //                     list.add(tile);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+        return lines;
+    }
 
-    // public List<String> rookMovement(Piece rook) {
-    //     List<String> possibleMoves = new ArrayList<>();
+    public List<Tile> getCross(Tile start){
+        List<Tile> lines = new ArrayList<>();
 
-    //     getPlus(rookLocation, possibleMoves);
+        lines.addAll(getLine(start, 1, 1)); // right up
+        lines.addAll(getLine(start, 1, -1)); //  right down
+        lines.addAll(getLine(start, -1, 1)); // left up
+        lines.addAll(getLine(start, -1, -1)); // left down
 
-    //     Collections.sort(possibleMoves);
-    //     System.out.println(Arrays.toString(possibleMoves.toArray()));
-    //     return possibleMoves;
-    // }
+        return lines;
+    }
 
-    // public List<String> knightMovement(String knightLocation) {
-    //     List<String> possibleMoves = new ArrayList<>();
+    // gets a square from the perspective of the location
+    public List<Tile> getSquare(Tile piece, int length) {
+        int start = 0 - (length/2);
+        int end = 0 + (length/2) + 1;
+        List<Tile> square = new ArrayList<>();
+        for (int col = start; col < end; col++) {
+            for (int row = start; row < end; row++) {
+                Tile tile = tileMath(piece, col, row);
+                if (tile != null && tile != piece) {
+                    if (tile.piece == null || (tile.piece != null && (tile.piece.side) != piece.piece.side && tile.piece.side != 'N')) {
+                        square.add(tile);
+                    }
+                }
+            }
+        }
 
-    //     List<String> unwanted = new ArrayList<>();
+        return square;
+    }
 
-    //     getPlus(knightLocation, unwanted); // gets all elements in a plus so we can remove them to create the knight circle
-    //     getCross(knightLocation, unwanted); // gets all elements in a cross so we can remove them to create the knight circle
-    //     getSquare(knightLocation, unwanted, -1, 2); // we need to remove the inner square since we use the square function for the knight
+    public List<Tile> rookMovement(Tile tile_with_rook) {
+        List<Tile> possibleMoves = new ArrayList<>();
 
-    //     getSquare(knightLocation, possibleMoves, -2, 3); // uses the square function to get a 5 length square
+        possibleMoves.addAll(getPlus(tile_with_rook));
 
-    //     possibleMoves.removeAll(unwanted); // remove the cross plus and inner square to leave a circle
+        return possibleMoves;
+    }
 
-    //     Collections.sort(possibleMoves);
-    //     System.out.println(Arrays.toString(possibleMoves.toArray()));
-    //     return possibleMoves;
-    // }
+    public List<Tile> knightMovement(Tile tile_with_knight) {
+        List<Tile> possibleMoves = new ArrayList<>();
 
-    // public List<String> bishopMovement(Piece bishop) {
-    //     List<String> possibleMoves = new ArrayList<>();
+        List<Tile> unwanted = new ArrayList<>();
 
-    //     getCross(bishopLocation, possibleMoves);
+        unwanted.addAll(getPlus(tile_with_knight)); // gets all elements in a plus so we can remove them to create the knight circle
+        unwanted.addAll(getCross(tile_with_knight)); // gets all elements in a cross so we can remove them to create the knight circle
+        unwanted.addAll(getSquare(tile_with_knight, 3)); // we need to remove the inner square since we use the square function for the knight
 
-    //     Collections.sort(possibleMoves);
-    //     System.out.println(Arrays.toString(possibleMoves.toArray()));
-    //     return possibleMoves;
-    // }
+        possibleMoves.addAll(getSquare(tile_with_knight, 5)); // uses the square function to get a 5 length square
 
-    // public List<String> queenMovement(Piece queen) {
-    //     List<String> possibleMoves = new ArrayList<>();
+        possibleMoves.removeAll(unwanted); // remove the cross plus and inner square to leave a circle
 
-    //     // use the cross and plus together to get movement range
-    //     getPlus(queenLocation, possibleMoves);
-    //     getCross(queenLocation, possibleMoves);
+        return possibleMoves;
+    }
 
-    //     Collections.sort(possibleMoves);
-    //     System.out.println(Arrays.toString(possibleMoves.toArray()));
-    //     return possibleMoves;
-    // }
+    public List<Tile> bishopMovement(Tile tile_with_bishop) {
+        List<Tile> possibleMoves = new ArrayList<>();
 
-    // public List<String> kingMovement(Piece king) {
-    //     List<String> possibleMoves = new ArrayList<>();
+        possibleMoves.addAll(getCross(tile_with_bishop));
 
-    //     getSquare(kingLocation, possibleMoves, -1, 2); // get a 3 length square
+        return possibleMoves;
+    }
 
-    //     Collections.sort(possibleMoves);
-    //     System.out.println(Arrays.toString(possibleMoves.toArray()));
-    //     return possibleMoves;
-    // }
+    public List<Tile> queenMovement(Tile tile_with_queen) {
+        List<Tile> possibleMoves = new ArrayList<>();
+
+        // use the cross and plus together to get movement range
+        possibleMoves.addAll(getPlus(tile_with_queen));
+        possibleMoves.addAll(getCross(tile_with_queen));
+
+        return possibleMoves;
+    }
+
+    public List<Tile> kingMovement(Tile tile_with_king) {
+        List<Tile> possibleMoves = new ArrayList<>();
+
+        possibleMoves.addAll(getSquare(tile_with_king, 3)); // get a 3 length square
+
+        return possibleMoves;
+    }
 
 }
--- Chess.java
+++ Chess.java
@@ -1,23 +1,7 @@
-import java.util.List;
+public class Chess {
 
-class Chess {
-    public static void main(String[] args) { 
-        Board board_manager = new Board();
-
-        board_manager.printBoard();
-
-        List<Tile> possibleMoves = board_manager.getLine(Board.board[6][0], 0, -1);
+    public static void main(String[] args) {
         
-        for(int i=0;i<possibleMoves.size();i++){
-            System.out.println(possibleMoves.get(i).location);
-        } 
-    
-
-        // board_manager.pawnMovement("A2");
-        // board_manager.pawnMovement("A7");
-
-        //board_manager.move("A8", "C5");
-
-        //board_manager.rookMovement("C5");
     }
+
 }
